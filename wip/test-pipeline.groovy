10239e68df3f0e6b921074e1af0686d64405e93a
Tianon Gravi <admwiggin@gmail.com>
2016-09-10 22:16:46 -0700

    Testing more things

--- wip/test-pipeline.groovy
+++ wip/test-pipeline.groovy
@@ -1,3 +1,5 @@
 node {
-	echo(getClass().protectionDomain.codeSource.location.path)
+	git 'https://github.com/docker-library/oi-janky-groovy.git'
+	def a = load('wip/test-load.groovy')
+	echo(a.@a)
 }
