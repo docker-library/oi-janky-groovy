b5327cd926f12a6d0bae885e9873e0f11fb1ceb7
Tianon Gravi <admwiggin@gmail.com>
2016-09-12 10:36:41 -0700

    Start fleshing out target-pipeline.groovy

--- update.sh/target-pipeline.groovy
+++ update.sh/target-pipeline.groovy
@@ -1,6 +1,15 @@
+def vars = fileLoader.fromGit(
+	'update.sh/vars.groovy', // script
+	'https://github.com/docker-library/oi-janky-groovy.git', // repo
+	'master', // branch
+	null, // credentialsId
+	'', // node/label
+)
+def repoMeta = vars.repoMeta(env.JOB_BASE_NAME)
+
 // TODO actually invoke "update.sh", etc
 node {
 	stage('Test') {
-		sh 'echo test'
+		echo(repoMeta['url'])
 	}
 }
