f39900617f0b9317d179bb9c89be1c1bc66fc63b
Tianon Gravi <admwiggin@gmail.com>
2016-09-12 10:11:33 -0700

    Do a little looping

--- update.sh/dsl.groovy
+++ update.sh/dsl.groovy
@@ -1,24 +1,26 @@
 import vars
 
 // TODO make lots of these
-pipelineJob('wordpress') {
-	logRotator { daysToKeep(4) }
-	triggers {
-		//cron('H H/6 * * *')
-	}
-	definition {
-		cpsScm {
-			scm {
-				git {
-					remote {
-						url('https://github.com/docker-library/oi-janky-groovy.git')
+for (repo in vars.repos) {
+	pipelineJob(repo) {
+		logRotator { daysToKeep(4) }
+		triggers {
+			//cron('H H/6 * * *')
+		}
+		definition {
+			cpsScm {
+				scm {
+					git {
+						remote {
+							url('https://github.com/docker-library/oi-janky-groovy.git')
+						}
+						extensions {
+							cleanAfterCheckout()
+							relativeTargetDirectory('oi-janky-groovy')
+						}
 					}
-					extensions {
-						cleanAfterCheckout()
-						relativeTargetDirectory('oi-janky-groovy')
-					}
+					scriptPath('oi-janky-groovy/update.sh/target-pipeline.groovy')
 				}
-				scriptPath('oi-janky-groovy/update.sh/target-pipeline.groovy')
 			}
 		}
 	}
