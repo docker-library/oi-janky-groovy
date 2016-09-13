e8df3a32cb0a64cb1c407ff2c4f129d228215ac4
Tianon Gravi <admwiggin@gmail.com>
2016-09-13 15:21:44 -0700

    Split "docker build" into a separate stage

--- docs/labeler-pipeline.groovy
+++ docs/labeler-pipeline.groovy
@@ -24,10 +24,13 @@ node {
 	}
 
 	dir('labeler') {
+		stage('Build') {
+			sh 'docker build -t docker-library-issue-labeler .'
+		}
+
 		withCredentials([[$class: 'StringBinding', credentialsId: 'github-token-docker-library-bot', variable: 'GITHUB_TOKEN']]) {
 			stage('Label') {
 				sh '''
-					docker build -t docker-library-issue-labeler .
 					set +x
 					docker run --rm \\
 						docker-library-issue-labeler \\
