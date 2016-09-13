08c307a47e6cdee22b5a686e92d9f98097c13a5a
Tianon Gravi <admwiggin@gmail.com>
2016-09-13 15:19:13 -0700

    Add new "labeler" pipeline

---
+++ docs/labeler-pipeline.groovy
@@ -0,0 +1,42 @@
+node {
+	stage('Checkout') {
+		checkout(
+			poll: true,
+			scm: [
+				$class: 'GitSCM',
+				userRemoteConfigs: [[
+					url: 'https://github.com/yosifkit/official-images-issue-labeler.git',
+				]],
+				branches: [[name: '*/master']],
+				extensions: [
+					[
+						$class: 'CleanCheckout',
+					],
+					[
+						$class: 'RelativeTargetDirectory',
+						relativeTargetDir: 'labeler',
+					],
+				],
+				doGenerateSubmoduleConfigurations: false,
+				submoduleCfg: [],
+			],
+		)
+	}
+
+	dir('labeler') {
+		withCredentials([[$class: 'StringBinding', credentialsId: 'github-token-docker-library-bot', variable: 'GITHUB_TOKEN']]) {
+			stage('Label') {
+				sh '''
+					docker build -t docker-library-issue-labeler .
+					set +x
+					docker run --rm \\
+						docker-library-issue-labeler \\
+						--token "$GITHUB_TOKEN" \\
+						--owner docker-library \\
+						--repo official-images \\
+						--state open
+				'''
+			}
+		}
+	}
+}
