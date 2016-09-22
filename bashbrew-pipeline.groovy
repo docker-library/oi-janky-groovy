6f0c774e19d09af2fd8ce836f258834bcf34da37
Tianon Gravi <admwiggin@gmail.com>
2016-09-22 13:10:53 -0700

    Add new "bashbrew" pipeline

---
+++ bashbrew-pipeline.groovy
@@ -0,0 +1,59 @@
+node {
+	stage('Checkout') {
+		checkout(
+			poll: true,
+			scm: [
+				$class: 'GitSCM',
+				userRemoteConfigs: [[
+					url: 'https://github.com/docker-library/official-images.git',
+				]],
+				branches: [[name: '*/master']],
+				extensions: [
+					[
+						$class: 'RelativeTargetDirectory',
+						relativeTargetDir: 'oi',
+					],
+					[
+						$class: 'CleanCheckout',
+					],
+					[
+						$class: 'PathRestriction',
+						excludedRegions: '',
+						includedRegions: 'bashbrew/go',
+					],
+				],
+				doGenerateSubmoduleConfigurations: false,
+				submoduleCfg: [],
+			],
+		)
+	}
+
+	wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
+		stage('Build') {
+			sh '''
+				docker build -t bashbrew --pull -q oi
+				docker build -t bashbrew:cross - <<-'EODF'
+					FROM bashbrew
+					WORKDIR bashbrew/go
+					RUN set -ex \\
+						&& rm -r bin \\
+						&& GOOS=darwin GOARCH=amd64 gb build \\
+						&& GOOS=linux GOARCH=amd64 gb build \\
+						&& GOOS=windows GOARCH=amd64 gb build \\
+						&& ls -l bin
+				EODF
+				rm -rf bin
+				docker run -i --rm bashbrew:cross tar -c bin \\
+					| tar -xv
+			'''
+		}
+	}
+
+	stage('Archive') {
+		archiveArtifacts(
+			artifacts: 'bin/*',
+			fingerprint: true,
+			onlyIfSuccessful: true,
+		)
+	}
+}
