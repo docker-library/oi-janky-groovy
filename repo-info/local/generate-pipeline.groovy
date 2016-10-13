09b761c5991fc00e972da45c7adda4b0453853d6
Tianon Gravi <admwiggin@gmail.com>
2016-10-13 15:57:22 -0700

    Add new WIP "repo-info/local" generator pipelines

---
+++ repo-info/local/generate-pipeline.groovy
@@ -0,0 +1,70 @@
+node('master') {
+	stage('Checkout') {
+		checkout(
+			poll: true,
+			scm: [
+				$class: 'GitSCM',
+				userRemoteConfigs: [[
+					url: 'https://github.com/docker-library/official-images.git',
+				]],
+				branches: [[name: '*/master']],
+				extensions: [
+					[$class: 'RelativeTargetDirectory', relativeTargetDir: 'oi'],
+					[$class: 'CleanCheckout'],
+				],
+				doGenerateSubmoduleConfigurations: false,
+				submoduleCfg: [],
+			],
+		)
+	}
+
+	def repos = []
+	stage('Gather') {
+		repos = sh(
+			returnStdout: true,
+			script: '''
+				export BASHBREW_LIBRARY="$PWD/oi/library"
+				bashbrew list --all --repos
+			''',
+		).trim().tokenize('\n')
+	}
+
+	stage('Generate') {
+		def dsl = ''
+		for (repo in repos) {
+			dsl += """
+				pipelineJob('${repo}') {
+					logRotator { daysToKeep(4) }
+					concurrentBuild(false)
+					triggers {
+						//cron('H H * * *')
+					}
+					definition {
+						cpsScm {
+							scm {
+								git {
+									remote {
+										url('https://github.com/docker-library/oi-janky-groovy.git')
+									}
+									branch('*/master')
+									extensions {
+										cleanAfterCheckout()
+										relativeTargetDirectory('oi-janky-groovy')
+									}
+								}
+								scriptPath('oi-janky-groovy/repo-info/local/target-pipeline.groovy')
+							}
+						}
+					}
+				}
+			"""
+		}
+		echo(dsl)
+		jobDsl(
+			lookupStrategy: 'SEED_JOB',
+			removedJobAction: 'DELETE',
+			removedViewAction: 'DELETE',
+			scriptText: dsl,
+		)
+	}
+}
---
+++ repo-info/local/target-pipeline.groovy
@@ -0,0 +1,135 @@
+def repo = env.JOB_BASE_NAME
+
+node {
+	stage('Checkout') {
+		checkout(
+			poll: false,
+			scm: [
+				$class: 'GitSCM',
+				userRemoteConfigs: [[
+					url: 'git@github.com:docker-library/repo-info.git',
+					credentialsId: 'docker-library-bot',
+					name: 'origin',
+				]],
+				branches: [[name: '*/master']],
+				extensions: [
+					[$class: 'RelativeTargetDirectory', relativeTargetDir: 'ri'],
+					[$class: 'CleanCheckout'],
+				],
+				doGenerateSubmoduleConfigurations: false,
+				submoduleCfg: [],
+			],
+		)
+		checkout(
+			poll: true,
+			scm: [
+				$class: 'GitSCM',
+				userRemoteConfigs: [[
+					url: 'https://github.com/docker-library/official-images.git',
+				]],
+				branches: [[name: '*/master']],
+				extensions: [
+					[$class: 'RelativeTargetDirectory', relativeTargetDir: 'oi'],
+					[$class: 'CleanCheckout'],
+				],
+				doGenerateSubmoduleConfigurations: false,
+				submoduleCfg: [],
+			],
+		)
+	}
+
+	def tags = []
+	stage('Gather') {
+		tags = sh(
+			returnStdout: true,
+			script: """
+				repo='${repo}'
+			""" + '''
+				export BASHBREW_LIBRARY="$PWD/oi/library"
+				bashbrew cat -f '
+					{{- range $.Entries -}}
+						{{- if not ($.SkipConstraints .) -}}
+							{{- join " " .Tags -}}
+							{{- "\\n" -}}
+						{{- end -}}
+					{{- end -}}
+				' "$repo"
+			''',
+		).trim().tokenize('\n')
+	}
+
+	wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) { dir('ri') {
+		stage('Prepare') {
+			sh '''
+				docker pull $(awk '$1 == "FROM" { print $2; exit }' Dockerfile.local)
+				sed -i 's/ --pull / /g' scan-local.sh
+				! grep -q -- '--pull' scan-local.sh
+			'''
+		}
+
+		stage('Scan') {
+			sh """
+				rm -rf 'repos/${repo}/local'
+				mkdir -p 'repos/${repo}/local'
+			"""
+
+			for (tagGroup in tags) {
+				def firstTagName = tagGroup[0]
+				def firstTag = repo + ':' + firstTagName
+				def firstTarget = "repos/${repo}/local/${firstTagName}.md"
+				stage(firstTag) {
+					def shells = [
+						"""
+							docker pull '${firstTag}'
+							./scan-local.sh '${firstTag}' > '${firstTarget}'
+						""",
+					]
+					for (int i = 1; i < tagGroup.size(); ++i) {
+						def nextTagName = tagGroup[i]
+						def nextTarget = "repos/${repo}/local/${nextTagName}.md"
+						shells << "cp '${firstTarget}' '${nextTarget}'"
+					}
+					sh(shells.join('\n'))
+				}
+			}
+		}
+
+		stage('Commit') {
+			sh """
+				git config user.name 'Docker Library Bot'
+				git config user.email 'github+dockerlibrarybot@infosiftr.com'
+
+				git add 'repos/${repo}/local' || :
+				git commit -m 'Run scan-local.sh on ${repo}:...' || :
+			"""
+		}
+
+		stage('Log') {
+			sh 'git log -p origin/master...HEAD'
+		}
+
+		def numCommits = sh(
+			returnStdout: true,
+			script: 'git rev-list --count origin/master...HEAD',
+		).trim().toInteger()
+		def hasChanges = (numCommits > 0)
+
+		stage('Push') {
+			if (hasChanges) {
+				sshagent(['docker-library-bot']) {
+					sh '''
+						# try catching up since this job takes so long to run sometimes
+						git checkout -- .
+						git clean -dfx .
+						git pull --rebase origin master || :
+
+						# fire away!
+						git push origin HEAD:master
+					'''
+				}
+			} else {
+				echo("No changes in ${repo}!  Skipping.")
+			}
+		}
+	} }
+}
