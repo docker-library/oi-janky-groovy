7e6ccb00e36eb82c217e7a8eea173b0ef2e001db
Tianon Gravi <admwiggin@gmail.com>
2017-05-07 18:02:13 -0700

    Fix typo

--- multiarch/generate-pipeline.groovy
+++ multiarch/generate-pipeline.groovy
@@ -42,7 +42,7 @@ node('master') {
 		for (arch in vars.arches) {
 			for (img in vars.archImages(arch)) {
 				def imageMeta = vars.imagesMeta[img]
-				if (!fileExists("oi-janky-groovy/${imageMeta['pipeline'}")) {
+				if (!fileExists("oi-janky-groovy/${imageMeta['pipeline']}")) {
 					echo("\n\nWARNING: '${imageMeta['pipeline']}' does not exist! (needed for '${img}')\n\n")
 					continue
 				}
