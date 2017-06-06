3157a7b56efadfd24c8e8d97d4b7da30be55dccd
Tianon Gravi <admwiggin@gmail.com>
2017-06-06 16:44:43 -0700

    Create more useful artifacts in our output

--- tianon/debuerreotype/arch-pipeline.groovy
+++ tianon/debuerreotype/arch-pipeline.groovy
@@ -58,6 +58,9 @@ node(multiarchVars.node(env.ACT_ON_ARCH, env.ACT_ON_IMAGE)) {
 					mkdir -p "$artifactsDir"
 					echo "$debuerreotypeVersion" > "$artifactsDir/debuerreotype-version"
 					echo "$serial" > "$artifactsDir/serial"
+					echo "$DPKG_ARCH" > "$artifactsDir/dpkg-arch"
+					"$debuerreotypeDir/scripts/.snapshot-url.sh" "$serial" > "$artifactsDir/snapshot-url"
+
 					"$debuerreotypeDir/build-all.sh" . "@$epoch"
 				'''
 			}
