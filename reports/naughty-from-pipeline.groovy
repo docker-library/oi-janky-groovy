f92d150995e1f1084575521bba88f92ffb7df4dc
Tianon Gravi <admwiggin@gmail.com>
2017-09-27 14:33:37 -0700

    Add new "naughty-from" pipeline

---
+++ reports/naughty-from-pipeline.groovy
@@ -0,0 +1,65 @@
+node {
+	env.BASHBREW_LIBRARY = env.WORKSPACE + '/oi/library'
+
+	checkout(
+		poll: true,
+		scm: [
+			$class: 'GitSCM',
+			userRemoteConfigs: [[
+				url: 'https://github.com/docker-library/official-images.git',
+			]],
+			branches: [[name: '*/master']],
+			extensions: [
+				[
+					$class: 'CleanCheckout',
+				],
+				[
+					$class: 'RelativeTargetDirectory',
+					relativeTargetDir: 'oi',
+				],
+				[
+					$class: 'PathRestriction',
+					excludedRegions: '',
+					includedRegions: 'library',
+				],
+			],
+			doGenerateSubmoduleConfigurations: false,
+			submoduleCfg: [],
+		],
+	)
+
+	repos = sh(returnStdout: true, script: '''
+		bashbrew list --all --repos
+	''').tokenize()
+
+	for (repo in repos) { withEnv(['repo=' + repo]) {
+		naughtyTags = sh(returnStdout: true, script: '''#!/usr/bin/env bash
+			set -Eeuo pipefail
+
+			for img in $(bashbrew list --uniq "$repo"); do
+				from="$(bashbrew from --apply-constraints --uniq "$img" 2>/dev/null | cut -d' ' -f2)"
+				case "$from" in
+					scratch|microsoft/*) continue ;;
+				esac
+				if ! bashbrew list "$from" &> /dev/null; then
+					echo "$img=$from"
+				fi
+			done
+		''').tokenize()
+
+		if (naughtyTags) {
+			stage(repo) {
+				str = "Naughty tags in the '${repo}' repo:\n"
+				for (tagPair in naughtyTags) {
+					tagPair = tagPair.tokenize('=')
+					tag = tagPair[0]
+					from = tagPair[1]
+					str += "\n - ${tag} (FROM ${from})"
+				}
+				echo(str)
+			}
+
+			currentBuild.result = 'FAILURE'
+		}
+	} }
+}
