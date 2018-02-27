fd3a4e31be5dda693ce4323441639501d16a4ba9
Tianon Gravi <admwiggin@gmail.com>
2018-02-27 13:08:24 -0800

    Add a TOC-updating pipeline

---
+++ docs/toc-pipeline.groovy
@@ -0,0 +1,106 @@
+properties([
+	buildDiscarder(logRotator(daysToKeepStr: '14')),
+	disableConcurrentBuilds(),
+	pipelineTriggers([
+		cron('H H * * *'),
+	]),
+])
+
+node {
+	stage('Checkout') {
+		checkout(
+			poll: true,
+			scm: [
+				$class: 'GitSCM',
+				userRemoteConfigs: [[
+					url: 'https://github.com/docker-library/official-images.git',
+				]],
+				branches: [[name: '*/master']],
+				extensions: [
+					[
+						$class: 'RelativeTargetDirectory',
+						relativeTargetDir: 'oi',
+					],
+					[
+						$class: 'CleanCheckout',
+					],
+					[
+						$class: 'PathRestriction',
+						excludedRegions: '',
+						includedRegions: [
+							'README.md',
+							'toc.sh',
+						].join('\n'),
+					],
+				],
+				doGenerateSubmoduleConfigurations: false,
+				submoduleCfg: [],
+			],
+		)
+		checkout(
+			poll: true,
+			scm: [
+				$class: 'GitSCM',
+				userRemoteConfigs: [[
+					url: 'git@github.com:docker-library/docs.git',
+					credentialsId: 'docker-library-bot',
+					name: 'origin',
+				]],
+				branches: [[name: '*/master']],
+				extensions: [
+					[
+						$class: 'CleanCheckout',
+					],
+					[
+						$class: 'RelativeTargetDirectory',
+						relativeTargetDir: 'd',
+					],
+					[
+						$class: 'PathRestriction',
+						excludedRegions: '',
+						includedRegions: [
+							'README.md',
+						].join('\n'),
+					],
+				],
+				doGenerateSubmoduleConfigurations: false,
+				submoduleCfg: [],
+			],
+		)
+	}
+
+	ansiColor('xterm') {
+		stage('official-images TOC') {
+			sh '''
+				oi/toc.sh oi/README.md
+			'''
+		}
+		stage('docs TOC') {
+			sh '''
+				oi/toc.sh d/README.md
+			'''
+		}
+
+		stage('Commit') {
+			sh('''
+				for dir in oi d; do
+					git -C "$dir" config user.name 'Docker Library Bot'
+					git -C "$dir" config user.email 'github+dockerlibrarybot@infosiftr.com'
+
+					git -C "$dir" add README.md || :
+					git -C "$dir" commit -m 'Update Table of Contents' || :
+				done
+			''')
+		}
+
+		sshagent(['docker-library-bot']) {
+			stage('Push') {
+				sh '''
+					for dir in oi d; do
+						git -C "$dir" push origin HEAD:refs/heads/master
+					done
+				'''
+			}
+		}
+	}
+}
