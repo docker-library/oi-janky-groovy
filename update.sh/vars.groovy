a5388afff3cb8214ebb762bce50cfbfc6764ed39
Tianon Gravi <admwiggin@gmail.com>
2018-05-09 12:33:34 -0700

    Rename piwik -> matomo (and alphabetize pierreozoux repos)

--- update.sh/vars.groovy
+++ update.sh/vars.groovy
@@ -159,17 +159,17 @@ def rawReposData = [
 	]],
 
 	// pierreozoux
-	['piwik', [
-		'url': 'git@github.com:piwik/docker-piwik.git',
-		'env': 'PIWIK_VERSION',
-	]],
-	['rocket.chat', [
-		'url': 'git@github.com:RocketChat/Docker.Official.Image.git',
-		'env': 'RC_VERSION',
+	['matomo', [
+		'url': 'git@github.com:matomo-org/docker.git',
+		'env': 'MATOMO_VERSION',
 	]],
 	['nextcloud', [
 		'url': 'git@github.com:nextcloud/docker.git',
 		'env': 'NEXTCLOUD_VERSION',
+	]],
+	['rocket.chat', [
+		'url': 'git@github.com:RocketChat/Docker.Official.Image.git',
+		'env': 'RC_VERSION',
 	]],
 ]
 
