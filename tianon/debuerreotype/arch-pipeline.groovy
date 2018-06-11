c3a7ecab65fbdc1a976d9a49b0032f968b473ae3
Tianon Gravi <admwiggin@gmail.com>
2018-06-11 08:19:30 -0700

    Calculate date values earlier

--- tianon/debuerreotype/arch-pipeline.groovy
+++ tianon/debuerreotype/arch-pipeline.groovy
@@ -31,6 +31,13 @@ env.TZ = 'UTC'
 
 node(multiarchVars.node(env.BUILD_ARCH, env.ACT_ON_IMAGE)) {
 	ansiColor('xterm') {
+		env.epoch = sh(returnStdout: true, script: 'date --date "$timestamp" +%s').trim()
+		env.serial = sh(returnStdout: true, script: 'date --date "@$epoch" +%Y%m%d').trim()
+		iso8601 = sh(returnStdout: true, script: 'date --date "@$epoch" --iso-8601=seconds').trim()
+
+		currentBuild.displayName = env.serial + ' (#' + currentBuild.number + ')'
+		currentBuild.description = '<code>@' + env.epoch + '</code><br /><code>' + iso8601 + '</code>'
+
 		env.debuerreotypeDir = env.WORKSPACE + '/debuerreotype'
 		dir(env.debuerreotypeDir) {
 			deleteDir()
@@ -46,13 +53,6 @@ node(multiarchVars.node(env.BUILD_ARCH, env.ACT_ON_IMAGE)) {
 				'''
 			}
 		}
-
-		env.epoch = sh(returnStdout: true, script: 'date --date "$timestamp" +%s').trim()
-		env.serial = sh(returnStdout: true, script: 'date --date "@$epoch" +%Y%m%d').trim()
-		iso8601 = sh(returnStdout: true, script: 'date --date "@$epoch" --iso-8601=seconds').trim()
-
-		currentBuild.displayName = env.serial + ' (#' + currentBuild.number + ')'
-		currentBuild.description = '<code>@' + env.epoch + '</code><br /><code>' + iso8601 + '</code>'
 
 		env.targetDir = env.WORKSPACE + '/debian'
 		env.artifactsDir = env.targetDir + '/' + env.serial + '/' + env.DPKG_ARCH
