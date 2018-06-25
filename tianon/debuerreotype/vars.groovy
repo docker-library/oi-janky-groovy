556e69f5650e39dea2def30a98341f5f56541b0e
Tianon Gravi <admwiggin@gmail.com>
2018-06-25 12:36:17 -0700

    Update debuerreotype to 0.8

--- tianon/debuerreotype/vars.groovy
+++ tianon/debuerreotype/vars.groovy
@@ -11,7 +11,7 @@ arches = [
 ] as Set
 
 // https://github.com/debuerreotype/debuerreotype/releases
-debuerreotypeVersion = '0.7'
+debuerreotypeVersion = '0.8'
 
 // build some arches explicitly elsewhere for speed/reliability
 buildArch = [
