b75522c77381c1815bb0ba9819938d16d148bc96
Tianon Gravi <admwiggin@gmail.com>
2019-09-03 14:34:09 -0700

    Initial attempt to generate individual "light" jobs for put-shared

---
+++ put-shared/generate-pipeline.groovy
@@ -0,0 +1,105 @@
+properties([
+	buildDiscarder(logRotator(numToKeepStr: '10')),
+	disableConcurrentBuilds(),
+	pipelineTriggers([
+		cron('H H * * *'),
+	]),
+])
+
+node {
+	env.BASHBREW_LIBRARY = env.WORKSPACE + '/oi/library'
+
+	stage('Checkout') {
+		checkout(
+			poll: true,
+			scm: [
+				$class: 'GitSCM',
+				userRemoteConfigs: [[
+					url: 'https://github.com/docker-library/official-images.git',
+				]],
+				branches: [[name: '*/master']],
+				extensions: [
+					[
+						$class: 'CleanCheckout',
+					],
+					[
+						$class: 'RelativeTargetDirectory',
+						relativeTargetDir: 'oi',
+					],
+					[
+						$class: 'PathRestriction',
+						excludedRegions: '',
+						includedRegions: 'library/.+',
+					],
+				],
+				doGenerateSubmoduleConfigurations: false,
+				submoduleCfg: [],
+			],
+		)
+	}
+
+	def dsl = ''
+
+	stage('Generate') {
+		def images = sh(returnStdout: true, script: '''
+			heavyRegex="$(grep -vE '^$|^#' oi/heavy-hitters.txt | paste -sd '|')"
+			bashbrew list --all --repos | grep -vE "^($heavyRegex)(:|\\$)"
+		''').trim().tokenize()
+
+		dsl += '''
+			def images = [
+		'''
+		for (image in images) {
+			dsl += """
+				'${image}',
+			"""
+		}
+		dsl += '''
+			]
+
+			for (image in images) {
+				pipelineJob("light/${image}") {
+					logRotator { daysToKeep(14) }
+					// TODO concurrentBuild(false)
+					// see https://issues.jenkins-ci.org/browse/JENKINS-31832?focusedCommentId=343307&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-343307
+					configure { it / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.DisableConcurrentBuildsJobProperty' { } }
+					triggers {
+						cron('H * * * *')
+					}
+					definition {
+						cpsScm {
+							scm {
+								git {
+									remote {
+										url('https://github.com/docker-library/oi-janky-groovy.git')
+									}
+									branch('*/master')
+									extensions {
+										cleanAfterCheckout()
+									}
+								}
+								scriptPath('put-shared/target-pipeline.groovy')
+							}
+						}
+					}
+					configure {
+						it / definition / lightweight(true)
+					}
+				}
+			}
+		'''
+	}
+
+	stage('Echo') {
+		echo(dsl)
+	}
+
+	stage('DSL') {
+		jobDsl(
+			lookupStrategy: 'SEED_JOB',
+			removedJobAction: 'DELETE',
+			removedViewAction: 'DELETE',
+			scriptText: dsl,
+		)
+	}
+}
---
+++ put-shared/target-pipeline.groovy
@@ -0,0 +1,76 @@
+// properties are set via "generate-pipeline.groovy" (jobDsl)
+
+// we can't use "load()" here because we don't have a file context (or a real checkout of "oi-janky-groovy" -- the pipeline plugin hides that checkout from the actual pipeline execution)
+def vars = fileLoader.fromGit(
+	'multiarch/vars.groovy', // script
+	'https://github.com/docker-library/oi-janky-groovy.git', // repo
+	'master', // branch
+	null, // credentialsId
+	'master', // node/label
+)
+
+env.ACT_ON_IMAGE = env.JOB_BASE_NAME // "memcached", etc
+env.PUSH_TO_NAMESPACE = 'library'
+
+env.BASHBREW_ARCH_NAMESPACES = vars.archNamespaces()
+
+node {
+	env.BASHBREW_LIBRARY = env.WORKSPACE + '/oi/library'
+
+	stage('Checkout') {
+		checkout(
+			poll: true,
+			scm: [
+				$class: 'GitSCM',
+				userRemoteConfigs: [[
+					url: 'https://github.com/docker-library/official-images.git',
+				]],
+				branches: [[name: '*/master']],
+				extensions: [
+					[
+						$class: 'CleanCheckout',
+					],
+					[
+						$class: 'RelativeTargetDirectory',
+						relativeTargetDir: 'oi',
+					],
+					[
+						$class: 'PathRestriction',
+						excludedRegions: '',
+						includedRegions: 'library/.+',
+					],
+				],
+				doGenerateSubmoduleConfigurations: false,
+				submoduleCfg: [],
+			],
+		)
+		checkout(
+			scm: [
+				$class: 'GitSCM',
+				userRemoteConfigs: [[
+					url: 'https://github.com/docker-library/perl-bashbrew.git',
+				]],
+				branches: [[name: '*/master']],
+				extensions: [
+					[
+						$class: 'CleanCheckout',
+					],
+					[
+						$class: 'RelativeTargetDirectory',
+						relativeTargetDir: 'perl',
+					],
+				],
+				doGenerateSubmoduleConfigurations: false,
+				submoduleCfg: [],
+			],
+		)
+	}
+
+	withCredentials([string(credentialsId: 'dockerhub-public-proxy', variable: 'DOCKERHUB_PUBLIC_PROXY')]) {
+		stage('Put Shared') {
+			sh '''
+				perl/put-multiarch.sh "$PUSH_TO_NAMESPACE/$ACT_ON_IMAGE"
+			'''
+		}
+	}
+}
