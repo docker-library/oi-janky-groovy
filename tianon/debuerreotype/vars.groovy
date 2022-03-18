d483b1cba9ba71e98397cf8e8718e1791a8018e1
Tianon Gravi <admwiggin@gmail.com>
2022-03-18 15:12:55 -0700

    Update to debuerreotype 0.14

--- tianon/debuerreotype/vars.groovy
+++ tianon/debuerreotype/vars.groovy
@@ -13,8 +13,8 @@ arches = [
 ] as Set
 
 // https://github.com/debuerreotype/debuerreotype/releases
-debuerreotypeVersion = '0.13'
-debuerreotypeExamplesCommit = '347363625abde3b06f4fc6790f6d13a70d1cf570' // to pull in a newer commit of examples/ (can be even with debuerreotypeVersion)
+debuerreotypeVersion = '0.14'
+debuerreotypeExamplesCommit = '0.14' // to pull in a newer commit of examples/ (can be even with debuerreotypeVersion)
 
 // build some arches explicitly elsewhere for speed/reliability
 buildArch = [
