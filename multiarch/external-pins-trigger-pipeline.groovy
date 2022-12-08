11cc2e3a208c44bf25e6cb35a86cbbb771ef7e52
Tianon Gravi <admwiggin@gmail.com>
2022-12-08 15:54:40 -0800

    Rejigger so things actually work correctly

--- multiarch/external-pins-trigger-pipeline.groovy
+++ multiarch/external-pins-trigger-pipeline.groovy
@@ -43,41 +43,50 @@ node(vars.node(env.ACT_ON_ARCH, 'external-pins-trigger')) {
 	dir('oi') {
 		pins = sh(returnStdout: true, script: '.external-pins/list.sh').tokenize()
 
-		for (pin in pins) { stage(pin) { withEnv(['PIN=' + pin]) {
-			children = sh(returnStdout: true, script: '''#!/usr/bin/env bash
+		for (pin in pins) { withEnv(['PIN=' + pin]) {
+			// start with a rough check for "does this architecture even *have* this parent?"
+			if (0 == sh(returnStatus: true, script: '''#!/usr/bin/env bash
 				set -Eeuo pipefail
 				set -x
+				bashbrew children --arch-filter --depth=-1 "$PIN"
+			''')) {
+				stage(pin) {
+					children = sh(returnStdout: true, script: '''#!/usr/bin/env bash
+						set -Eeuo pipefail
+						set -x
 
-				bashbrew children --arch-filter --depth 1 "$PIN" \
-					| cut -d: -f1 \
-					| sort -u
-			''').tokenize()
+						bashbrew children --arch-filter --depth 1 "$PIN" \
+							| cut -d: -f1 \
+							| sort -u
+					''').tokenize()
 
-			for (child in children) { withEnv(['CHILD=' + child]) {
-				if (0 != sh(returnStatus: true, script: '''#!/usr/bin/env bash
-					set -Eeuo pipefail
-					set -x
-					commit="$(wget -qO- "https://doi-janky.infosiftr.net/job/multiarch/job/$ACT_ON_ARCH/job/$CHILD/lastSuccessfulBuild/artifact/build-info/commit.txt")"
-					[ -n "$commit" ]
-					file="$(.external-pins/file.sh "$PIN")"
-					[ -s "$file" ]
-					dir="$(dirname "$file")"
-					file="$(basename "$file")"
-					touchingCommits="$(git -C "$dir" log --oneline "$commit...HEAD" -- "$file")"
-					[ -z "$touchingCommits" ]
-				''')) {
-					catchError(stageResult: 'FAILURE') {
-						build(
-							job: child,
-							quietPeriod: 15 * 60, // 15 minutes
-							wait: false,
-						)
+					for (child in children) { withEnv(['CHILD=' + child]) {
+						if (0 != sh(returnStatus: true, script: '''#!/usr/bin/env bash
+							set -Eeuo pipefail
+							set -x
+							commit="$(wget -qO- "https://doi-janky.infosiftr.net/job/multiarch/job/$ACT_ON_ARCH/job/$CHILD/lastSuccessfulBuild/artifact/build-info/commit.txt")"
+							[ -n "$commit" ]
+							file="$(.external-pins/file.sh "$PIN")"
+							[ -s "$file" ]
+							dir="$(dirname "$file")"
+							file="$(basename "$file")"
+							touchingCommits="$(git -C "$dir" log --oneline "$commit...HEAD" -- "$file")"
+							[ -z "$touchingCommits" ]
+						''')) {
+							catchError(stageResult: 'FAILURE') {
+								build(
+									job: child,
+									quietPeriod: 15 * 60, // 15 minutes
+									wait: false,
+								)
 
-						// also mark the build as unstable so it's obvious which trigger jobs actually triggered builds
-						currentBuild.result = 'UNSTABLE'
-					}
+								// also mark the build as unstable so it's obvious which trigger jobs actually triggered builds
+								currentBuild.result = 'UNSTABLE'
+							}
+						}
+					} }
 				}
-			} }
-		} } }
+			}
+		} }
 	}
 }
